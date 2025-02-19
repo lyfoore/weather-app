88fd41e139d829a52ddfb4a86f6f5464a6f91f92
Maxim Zheribor <lyfoore@gmail.com>
2025-02-19 22:15:22 +0700

    made working docker-compose

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [time, setTime] = useState('');
+
+  useEffect(() => {
+    fetch('/api/time')
+      .then(response => response.json())
+      .then(data => setTime(data.time))
+      .catch(error => console.error('Error:', error));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Current time from backend:</h1>
+      <p>{time || "Loading..."}</p>
     </div>
   );
 }
